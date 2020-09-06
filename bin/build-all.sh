95c9dd0c38a4778424eae82349f7a5cc65219753
Junmin Ahn <junminahn@outlook.com>
2020-09-06 14:55:32 -0700

    override index.js when building

--- bin/build-all.sh
+++ bin/build-all.sh
@@ -2,9 +2,21 @@
 
 set -o pipefail
 
+rm -rf build-tmp
+rm -rf build-tmp2
+
+mv -n src/index.js src/index.tmp
+
+echo "require('./base');" >src/index.js
 yarn build
 mv build build-tmp
-mv build-tmp/index.html build-tmp/admin.html
-yarn build:coreui
+
+echo "require('./coreui');" >src/index.js
+yarn build
+cp -r build build-tmp2
+
+mv -n src/index.tmp src/index.js
+
+mv build/index.html build/admin.html
 node scripts/merge-json.js
 rsync -a build-tmp/ build
