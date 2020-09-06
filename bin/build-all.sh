862c4b373e4ad258cd54b754890f874ee22dd1ed
Junmin Ahn <junminahn@outlook.com>
2020-09-06 15:08:39 -0700

    optimize build script

--- bin/build-all.sh
+++ bin/build-all.sh
@@ -2,21 +2,24 @@
 
 set -o pipefail
 
-rm -rf build-tmp
-rm -rf build-tmp2
+build() {
+    rm -rf build-${1}
+    echo "require('./${1}');" >src/index.js
+    yarn build
+    cp -r build build-${1}
+}
+
+log_error() {
+    printf '\e[31mERROR: %s\n\e[39m' "$1" >&2
+}
 
 mv -n src/index.js src/index.tmp
 
-echo "require('./base');" >src/index.js
-yarn build
-mv build build-tmp
-
-echo "require('./coreui');" >src/index.js
-yarn build
-cp -r build build-tmp2
+build base
+build coreui
 
 mv -n src/index.tmp src/index.js
 
 mv build/index.html build/admin.html
 node scripts/merge-json.js
-rsync -a build-tmp/ build
+rsync -a build-base/ build
